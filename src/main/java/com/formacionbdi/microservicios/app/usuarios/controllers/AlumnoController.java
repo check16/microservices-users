cd87533f55b4b536acff5e6b91380ee4506985ab
Antonio Sanchez <antonio.san.87@hotmail.com>
2021-09-23 18:18:50 +0200

    Modified AlumnoController from CommonController

--- src/main/java/com/formacionbdi/microservicios/app/usuarios/controllers/AlumnoController.java
+++ src/main/java/com/formacionbdi/microservicios/app/usuarios/controllers/AlumnoController.java
@@ -2,44 +2,22 @@ package com.formacionbdi.microservicios.app.usuarios.controllers;
 
 import com.formacionbdi.microservicios.app.usuarios.models.entity.Alumno;
 import com.formacionbdi.microservicios.app.usuarios.services.AlumnoService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.formacionbdi.microservicios.commons.controllers.CommonController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
 @RestController
-public class AlumnoController {
-
-    @Autowired
-    private AlumnoService alumnoService;
-
-    @GetMapping
-    public ResponseEntity<?> listar() {
-        return ResponseEntity.ok(alumnoService.findAll());
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<?> ver(@PathVariable Long id) {
-        Optional<Alumno> optionalAlumno = alumnoService.findById(id);
-        if (optionalAlumno.isEmpty()) {
-            return ResponseEntity.notFound()
-                                 .build();
-        }
-        return ResponseEntity.ok(alumnoService.findById(id));
-    }
-
-    @PostMapping
-    public ResponseEntity<?> crear(@RequestBody Alumno alumno) {
-        Alumno alumnoDB = alumnoService.save(alumno);
-        return ResponseEntity.status(HttpStatus.CREATED)
-                             .body(alumnoDB);
-    }
+public class AlumnoController extends CommonController<Alumno, AlumnoService> {
 
     @PutMapping("/{id}")
     public ResponseEntity<?> editar(@RequestBody Alumno alumno, @PathVariable Long id) {
-        Optional<Alumno> optionalAlumno = alumnoService.findById(id);
+        Optional<Alumno> optionalAlumno = service.findById(id);
         if (optionalAlumno.isEmpty()) {
             return ResponseEntity.notFound()
                                  .build();
@@ -50,13 +28,6 @@ public class AlumnoController {
         alumnoDB.setEmail(alumno.getEmail());
 
         return ResponseEntity.status(HttpStatus.CREATED)
-                             .body(alumnoService.save(alumnoDB));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> eliminar(@PathVariable Long id) {
-        alumnoService.deleteById(id);
-        return ResponseEntity.noContent()
-                             .build();
+                             .body(service.save(alumnoDB));
     }
 }
